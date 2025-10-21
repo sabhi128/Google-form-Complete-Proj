49dec8aa01627dc58167fa268e305076aa51d461
sabhi128 <sabhiahm12@gmail.com>
2025-10-21 23:54:16 +0500

    some issues fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,18 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // ensure correct paths on Vercel
-  build: {
-    outDir: 'dist', // default, but let’s be explicit
-  },
+  base: './',
   server: {
     port: 4000,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
         changeOrigin: true,
-        secure: false,
-      },
+        secure: false
+      }
     },
-  },
+    historyApiFallback: true
+  }
 })
